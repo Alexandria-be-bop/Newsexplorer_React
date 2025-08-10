495977533c01279b39924c304c0e1b9b87d8155c
Liam Devine <skynotebook@proton.me>
2025-08-10 17:29:57 -0400

    css styling

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,17 +2,17 @@ import "./SearchForm.css";
 
 function SearchForm() {
   return (
-    <div className="SearchForm">
-      <h2 className="SearchForm__title">What's going on in the world?</h2>
-      <p className="SearchForm__subtitle">
+    <div className="search-form">
+      <h2 className="search-form__title">What's going on in the world?</h2>
+      <p className="search-form__subtitle">
         Find the latest news on any topic and save them in your personal account. 
       </p>
       <div>
-        <form className="SearchForm__form">
-          <input className="SearchForm__input" placeholder="Enter topic"></input>
+        <form className="search-form__form">
+          <input className="search-form__input" placeholder="Enter topic"></input>
           <button
             type="submit"
-            className="SearchForm__button"
+            className="search-form__button"
           >Search</button>
         </form>
       </div>
