dc52afd078bb92f34065ee66a2856dda24d10cf5
Liam Devine <skynotebook@proton.me>
2025-08-12 19:30:45 -0400

    added backup fonts

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,15 @@
 import About from "../About/About";
 import SearchForm from "../SearchForm/SearchForm";
 import "./Main.css";
+import Preloader from "../Preloader/Preloader";
 
 function Main() {
+  const isLoading = true;
+
   return (
     <div className="main">
       <SearchForm />
+      <div>{isLoading ? <Preloader /> : <Content />}</div>
       <About />
     </div>
   );
