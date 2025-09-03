dc4c94b37396c6ee7d51f4476a427a2ea1d0a61c
Liam Devine <skynotebook@proton.me>
2025-09-02 20:48:30 -0400

    URL changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/stage-1-frontend-and-api/",
+  base: "/Newsexplorer_React/",
   server: {
     port: 3000,
   },
